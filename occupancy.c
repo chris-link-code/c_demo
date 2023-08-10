ae54e24597583c4658047c37a2e450d9cbc9c58a
chris <er1293545211@live.com>
2023-08-10 22:41:29 +0800

    occupancy ok

--- occupancy.c
+++ occupancy.c
@@ -11,10 +11,29 @@ char *p1;
 // TODO 占用1G内存
 int main() {
     printf("%d\n", (1 << 30));
-    p1 = (char *) malloc((1 << 30) * sizeof(char));
+    printf("char: %d\n", sizeof(char));
+    printf("int: %d\n", sizeof(int));
+    p1 = (char *) malloc(1 << 30);
+
+    printf("memset\n");
+
+    // 向内存空间内填充数据
+    memset(p1, 0xFF, 1 << 30);
     sleep(30);
+
     printf("free\n");
+    // 释放内存
     free(p1);
     sleep(30);
     return 0;
 }
+
+/*#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+    int *a;
+    while (1) {
+        a = (int *) malloc(1000 * sizeof(int));
+    }
+}*/
