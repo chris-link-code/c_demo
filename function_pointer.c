b2c4ac24686644637b67bf82346884ab104d759c
chris <chris>
2024-06-30 13:10:16 +0800

    callback

--- function_pointer.c
+++ function_pointer.c
@@ -42,15 +42,15 @@ void rectangle_draw(shape_t *shape) {
 }
 
 /**
- * 定义了一个 shape 结构体，它有一个函数指针 draw，用于绘制该形状。
- * 我们还定义了两个形状：circle 和 rectangle，它们分别包含它们自己的属性和一个指向 shape 结构体的指针。
- * 每个形状都定义了自己的 draw 函数，用于绘制该形状。
- * 在 main 函数中，我们定义了一个 shape_t 类型的数组，其中包含一个 circle 和一个 rectangle。
- * 我们使用一个循环来遍历这个数组，并使用每个形状的 draw 函数来绘制该形状。
- * 注意，尽管 shapes 数组中的元素类型为 shape_t *，但我们仍然可以调用每个元素的 draw 函数，
- * 因为 circle 和 rectangle 都是从 shape_t 派生出来的，它们都包含一个 draw 函数指针。
- * 这个例子演示了如何使用函数指针来实现多态。
- * 尽管 C 语言不支持面向对象编程，但我们可以使用结构体和函数指针来实现类似的概念。
+ * 定义了一个 shape 结构体，它有一个函数指针 draw，用于绘制该形状
+ * 我们还定义了两个形状circle和rectangle，它们分别包含它们自己的属性和一个指向shape结构体的指针
+ * 每个形状都定义了自己的 draw 函数，用于绘制该形状
+ * 在 main 函数中，我们定义了一个 shape_t 类型的数组，其中包含一个 circle 和一个 rectangle
+ * 我们使用一个循环来遍历这个数组，并使用每个形状的 draw 函数来绘制该形状
+ * 注意，尽管 shapes 数组中的元素类型为 shape_t *，但我们仍然可以调用每个元素的 draw 函数
+ * 因为 circle 和 rectangle 都是从 shape_t 派生出来的，它们都包含一个 draw 函数指针
+ * 这个例子演示了如何使用函数指针来实现多态
+ * 尽管 C 语言不支持面向对象编程，但我们可以使用结构体和函数指针来实现类似的概念
  */
 int main() {
     circle_t circle =
